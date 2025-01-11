4c5f05e4eaf700a903ae0598db1b258b48d39862
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-01-11 16:14:09 +0100

    landing page edit

--- src/app/(festival)/page.jsx
+++ src/app/(festival)/page.jsx
@@ -4,32 +4,12 @@ import TicketCard from "@/components/TicketCard";
 import Image from "next/image";
 import hero from "@/assets/img/hero.png";
 import { MdKeyboardArrowDown } from "react-icons/md";
-import ScrollToButton from "@/components/lineup/ScrollToButton";
 
 export default function Home() {
   return (
     <main className="full-bleed">
       <section className="full-bleed gap-y-4">
-        <article className="full-bleed row-start-1 row-span-4">
-          <Image
-            className="full-bleed h-svh object-cover row-start-1 md:h-full"
-            src={hero}
-            alt="Image of FooFest participants. Photo by Roberto Rendon on Unsplash."
-            priority
-          ></Image>
-          <div className="col-start-2 row-start-1 self-center flow-space grid grid-cols-3">
-            <h1 className="heading-1 uppercase text-balance col-span-full py-4 md:py-0 ">
-              Claim your ticket for FooFest
-            </h1>
-            <MdKeyboardArrowDown
-              size={50}
-              className="text-global justify-self-center col-span-full md:hidden"
-            />
-            {/* <ScrollToButton scrollFromTop="1800" simple={true}>
-              <MdKeyboardArrowDown size={50} className="text-global" />
-            </ScrollToButton> */}
-          </div>
-        </article>
+        <Hero />
         <footer className="py-4 lg:col-start-2 lg:row-start-4 lg:row-span-2 lg:grid lg:grid-cols-3 lg:gap-6">
           <div className="grid grid-cols-1 place-content-start gap-4 sm:grid-cols-2 lg:col-start-2 lg:col-span-full lg:grid-cols-subgrid ">
             <TicketCard
@@ -48,5 +28,30 @@ export default function Home() {
         </footer>
       </section>
     </main>
+  );
+}
+
+function Hero() {
+  return (
+    <article className="full-bleed row-start-1 row-span-4">
+      <Image
+        className="full-bleed h-svh object-cover row-start-1 md:h-full"
+        src={hero}
+        alt="Image of FooFest participants. Photo by Roberto Rendon on Unsplash."
+        priority
+      ></Image>
+      <div className="col-start-2 row-start-1 self-center flow-space grid grid-cols-3">
+        <h1 className="heading-1 uppercase text-balance col-span-full py-4 md:py-0 ">
+          Claim your ticket for FooFest
+        </h1>
+        <MdKeyboardArrowDown
+          size={50}
+          className="text-global justify-self-center col-span-full md:hidden"
+        />
+        {/* <ScrollToButton scrollFromTop="1800" simple={true}>
+              <MdKeyboardArrowDown size={50} className="text-global" />
+            </ScrollToButton> */}
+      </div>
+    </article>
   );
 }
