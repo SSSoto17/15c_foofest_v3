45eda26c4d3b8397e7a25f7d26292fd989d53b2b
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-01-11 15:52:58 +0100

    lineup heading correction

--- src/components/lineup/LineupLayout.jsx
+++ src/components/lineup/LineupLayout.jsx
@@ -4,7 +4,7 @@ export default function LineupLayout({ category, children }) {
   return (
     <main className="grid gap-10">
       <section className="flow-space">
-        <h1 className="heading-title text-center">Line up</h1>
+        <h1 className="heading-title text-center">Lineup</h1>
         <LineupNav active={category}></LineupNav>
         {children}
       </section>
