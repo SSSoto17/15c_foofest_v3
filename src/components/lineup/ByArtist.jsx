09978d41df29a4f449691b27d7e3fb1b38080da7
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-01-11 15:38:22 +0100

    Dynamic images fixed

--- src/components/lineup/ByArtist.jsx
+++ src/components/lineup/ByArtist.jsx
@@ -1,9 +1,9 @@
 import ArtistCard from "@/components/lineup/ArtistCard";
 import Link from "next/link";
 
-const ByArtist = ({ artists, limit }) => {
-  const endpoint = process.env.FOO_FEST_API_URL;
+import { endpointAPI } from "@/lib/endpoints";
 
+const ByArtist = ({ artists, limit }) => {
   //Sorting artists
   // Useing method from the following link
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
@@ -31,12 +31,25 @@ const ByArtist = ({ artists, limit }) => {
     <section className="grid mb-16">
       <ul className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4">
         {artistsShowed.map((artist, i) => (
-          <ArtistCard key={i} name={artist.name} slug={artist.slug} img={artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`}></ArtistCard>
+          <ArtistCard
+            key={i}
+            name={artist.name}
+            slug={artist.slug}
+            img={
+              artist.logo.startsWith("https://")
+                ? artist.logo
+                : `${endpointAPI}/logos/${artist.logo}`
+            }
+          ></ArtistCard>
         ))}
       </ul>
 
       {limit < artists.length && (
-        <Link href={`/lineup/artists?limit=${newLoad}`} scroll={false} className="border-2 border-forest-600 body-copy text-forest-500 hover:text-forest-400 hover:border-forest-500 inline-block px-6 py-3 mt-8 place-self-center">
+        <Link
+          href={`/lineup/artists?limit=${newLoad}`}
+          scroll={false}
+          className="border-2 border-forest-600 body-copy text-forest-500 hover:text-forest-400 hover:border-forest-500 inline-block px-6 py-3 mt-8 place-self-center"
+        >
           Load more
         </Link>
       )}
