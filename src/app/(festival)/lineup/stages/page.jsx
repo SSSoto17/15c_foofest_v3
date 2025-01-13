699abb63e8938e79f1eac828065a6aa41fe62ccb
Sif Melkane <siha0004@stud.kea.dk>
2025-01-13 19:27:34 +0100

    Restructering schedule tables

--- src/app/(festival)/lineup/stages/page.jsx
+++ src/app/(festival)/lineup/stages/page.jsx
@@ -1,7 +1,8 @@
 import { getStages } from "@/lib/lineup";
-import LineupLayout from "@/components/lineup/LineupLayout";
 import Accordion from "@/components/Accordion";
-import StageSchedule from "@/components/lineup/schedule/StageSchedule";
+import ColSchedule from "@/components/lineup/schedule/ColSchedule";
+import HeaderSchedule from "@/components/lineup/schedule/HeaderSchedule";
+import TableCell from "@/components/lineup/schedule/TableCell";
 
 export default async function Stages() {
   const stages = await getStages();
@@ -12,14 +13,110 @@ export default async function Stages() {
   }));
 
   return (
-    <section className="grid gap-4">
-      {stageNames.map((stage, i) => {
+    <section className="grid gap-4 items-start">
+      {stageSchedule.map((stage, i) => {
+        const days = stage.days.mon;
+        console.log("HEJ:", days);
         return (
-          <Accordion key={i} variant="primary" label={stage} name="stage">
-            <StageSchedule data={stageSchedule[i].days}></StageSchedule>
+          <Accordion key={i} variant="primary" label={stage.name} name="stage">
+            <section className="grid grid-cols-[auto_1fr_1fr_1fr] grid-rows-[2fr_12fr]">
+              <ColSchedule data={stage.days.thu} time>
+                {/* {stage.days.thu.map((time, i) => (
+                  <li key={i}>
+                    <p className="text-res-sm sm:text-res-base">{time.start}</p>
+                  </li>
+                ))} */}
+              </ColSchedule>
+              <ColSchedule data={stage.days.thu} decoration="08">
+                Thursday
+              </ColSchedule>
+              <ColSchedule data={stage.days.fri} decoration="09">
+                Friday
+              </ColSchedule>
+              <ColSchedule data={stage.days.sat} decoration="10">
+                Thursday
+              </ColSchedule>
+            </section>
           </Accordion>
         );
       })}
+      <Accordion variant="primary" label="stage" name="stage">
+        <section className="grid grid-cols-4 grid-rows-[2fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr]">
+          <HeaderSchedule text1="Thursday" dec1="08" text2="Friday" dec2="09" text3="Saturday" dec3="10"></HeaderSchedule>
+          {/* <ul className="row-start-2 row-span-full col-span-2 grid grid-cols-1 grid-rows-subgrid">
+            {thu.map((time, i) => (
+              <TableCell key={i}>
+                <p className="text-res-sm sm:text-res-base">{time.start}</p>
+              </TableCell>
+            ))}
+          </ul> */}
+          <ul className="col-span-6 row-span-full grid grid-cols-subgrid grid-rows-subgrid">
+            {/* <ColSchedule acts={thu}></ColSchedule>
+            <ColSchedule acts={fri}></ColSchedule>
+            <ColSchedule acts={sat}></ColSchedule> */}
+          </ul>
+        </section>
+      </Accordion>
     </section>
+
+    //  <section className="grid gap-4">
+    //   {stageNames.map((stage, i) => {
+
+    //     return (
+    //       <Accordion key={i} variant="primary" label={stage} name="stage">
+    //         <section className="grid grid-cols-8 grid-rows-[2fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr]">
+    //           <HeaderSchedule text1="Thursday" dec1="08" text2="Friday" dec2="09" text3="Saturday" dec3="10"></HeaderSchedule>
+    //           <ul className="row-start-2 row-span-full col-span-2 grid grid-cols-1 grid-rows-subgrid">
+    //             {thu.map((time, i) => (
+    //               <TableCell key={i}>
+    //                 <p className="text-res-sm sm:text-res-base">{time.start}</p>
+    //               </TableCell>
+    //             ))}
+    //           </ul>
+    //           <ul className="col-span-6 row-span-full grid grid-cols-subgrid grid-rows-subgrid">
+    //             <ColSchedule acts={thu}></ColSchedule>
+    //             <ColSchedule acts={fri}></ColSchedule>
+    //             <ColSchedule acts={sat}></ColSchedule>
+    //           </ul>
+    //         </section>
+    //       </Accordion>
+    //     );
+    //   })}
+    // </section>
   );
 }
+
+function TabelCol({ children }) {
+  return (
+    <article>
+      <header>TitleSchedule</header>
+      <ul className="row-start-2 row-span-full col-span-2 grid grid-cols-1 grid-rows-subgrid">{children}</ul>
+    </article>
+  );
+}
+
+const StageSchedule = ({ data }) => {
+  console.log("Schedule DATA", data);
+
+  const thu = data.thu;
+  const fri = data.fri;
+  const sat = data.sat;
+
+  // return (
+  //   <section className="grid grid-cols-[0.5fr_1fr_1fr_1fr] grid-rows-[2fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr]">
+  //     <HeaderSchedule text1="Thursday" dec1="08" text2="Friday" dec2="09" text3="Saturday" dec3="10"></HeaderSchedule>
+  //     <ul className="row-start-2 row-span-full grid grid-cols-1 grid-rows-subgrid">
+  //       {thu.map((time, i) => (
+  //         <TableCell key={i}>
+  //           <p className="text-res-sm sm:text-res-base">{time.start}</p>
+  //         </TableCell>
+  //       ))}
+  //     </ul>
+  //     <ul className="col-start-2 col-span-full row-span-full grid grid-cols-subgrid grid-rows-subgrid">
+  //       <ColSchedule acts={thu}></ColSchedule>
+  //       <ColSchedule acts={fri}></ColSchedule>
+  //       <ColSchedule acts={sat}></ColSchedule>
+  //     </ul>
+  //   </section>
+  // );
+};
--- src/components/lineup/schedule/ColSchedule.jsx
+++ src/components/lineup/schedule/ColSchedule.jsx
@@ -1,39 +1,85 @@
 import { getArtists } from "@/lib/lineup";
-import TableCell from "./TableCell";
 import Link from "next/link";
 
-const ColSchedule = async ({ acts }) => {
+const ColSchedule = async ({ data, time, decoration, children }) => {
   const artists = await getArtists();
-  // console.log(artists);
+  console.log(artists);
   return (
-    <li className=" row-start-2 row-span-full grid grid-cols-1 grid-rows-subgrid ">
-      <ul className="row-span-full grid grid-rows-subgrid">
-        {acts.map((act, id) => {
-          const artistMatch = artists.find(({ name }) => name === act.act);
-          return act.act === "break" ? (
-            <TableCell key={id}></TableCell>
-          ) : act.cancelled ? (
-            <TableCell key={id}>
-              <Link href={`/lineup/artists/single/${artistMatch.slug}`}>
-                <p className="col-start-1 text-res-sm sm:text-res-base">
-                  {act.act}
-                </p>
-              </Link>
-              <p className="col-start-1 opacity-75 -rotate-12 text-res-xs sm:text-res-sm uppercase border-2 border-gold-600 text-gold-600 inline-block px-1 sm:px-2 sm:py-0.5">
-                Cancelled
-              </p>
-            </TableCell>
-          ) : (
-            <TableCell key={id}>
-              <Link href={`/lineup/artists/single/${artistMatch.slug}`}>
-                <p className="text-res-sm sm:text-res-base"> {act.act}</p>
-              </Link>
+    <article className="row-span-full grid grid-rows-subgrid">
+      <header>
+        <TitleSchedule>
+          {decoration && <p className="hidden font-display text-res-5xl md:block uppercase text-forest-900 row-start-1 row-span-4 col-start-1 col-span-3">{decoration}</p>}
+          <h2 className="-rotate-45 -translate-9 sm:-translate-12 md:translate-0 md:rotate-0 heading-5 row-start-3 row-span-2 col-start-2 col-span-full">{children}</h2>
+        </TitleSchedule>
+      </header>
+      <ul className="grid grid-rows-12">
+        {data.map((obj, id) => {
+          const artistMatch = artists.find(({ name }) => name === obj.act);
+          return (
+            <TableCell key={id} cancelled={!time && obj.cancelled}>
+              {time
+                ? obj.start
+                : !time &&
+                  obj.act !== "break" && (
+                    <Link href={`/lineup/artists/single/${artistMatch.slug}`} className="col-start-1 text-res-sm sm:text-res-base">
+                      {obj.act}
+                    </Link>
+                  )}
             </TableCell>
           );
         })}
       </ul>
-    </li>
+    </article>
   );
 };
 
 export default ColSchedule;
+
+const TitleSchedule = ({ children }) => {
+  return <div className="col-span-2 grid grid-cols-[1fr_1fr_1fr_4fr]  grid-rows-4">{children}</div>;
+};
+
+const TableCell = ({ cancelled, children }) => {
+  return (
+    <li className="pr-2 sm:p-2 border-t-2 border-border-global min-w-32">
+      {children}
+      {cancelled && <p className="col-start-1 opacity-75 -rotate-8 text-res-xs sm:text-res-sm uppercase border-2 border-gold-600 text-gold-600 max-w-min px-1 sm:px-2 sm:py-0.5">Cancelled</p>}
+    </li>
+  );
+};
+
+// const ColSchedule = async ({ acts }) => {
+//   const artists = await getArtists();
+//   // console.log(artists);
+//   return (
+//     <li className=" row-start-2 row-span-full grid grid-cols-1 grid-rows-subgrid ">
+//       <ul className="row-span-full grid grid-rows-subgrid">
+//         {acts.map((act, id) => {
+//           const artistMatch = artists.find(({ name }) => name === act.act);
+//           return act.act === "break" ? (
+//             <TableCell key={id}></TableCell>
+//           ) : act.cancelled ? (
+//             <TableCell key={id}>
+//               <Link href={`/lineup/artists/single/${artistMatch.slug}`}>
+//                 <p className="col-start-1 text-res-sm sm:text-res-base">
+//                   {act.act}
+//                 </p>
+//               </Link>
+//               <p className="col-start-1 opacity-75 -rotate-12 text-res-xs sm:text-res-sm uppercase border-2 border-gold-600 text-gold-600 inline-block px-1 sm:px-2 sm:py-0.5">
+//                 Cancelled
+//               </p>
+//             </TableCell>
+//           ) : (
+//             <TableCell key={id}>
+//               <Link href={`/lineup/artists/single/${artistMatch.slug}`}>
+//                 <p className="text-res-sm sm:text-res-base"> {act.act}</p>
+//               </Link>
+//             </TableCell>
+//           );
+//         })}
+//       </ul>
+//     </li>
+//   );
+// };
+
+// export default ColSchedule;
