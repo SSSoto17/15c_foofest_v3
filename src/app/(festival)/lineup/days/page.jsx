5f8316777f65c0227ac96e0af39a6742a317d7d3
Sif Melkane <siha0004@stud.kea.dk>
2025-01-13 19:32:13 +0100

    removing import of layout

--- src/app/(festival)/lineup/days/page.jsx
+++ src/app/(festival)/lineup/days/page.jsx
@@ -1,6 +1,5 @@
 import { getStages } from "@/lib/lineup";
 
-import LineupLayout from "@/components/lineup/LineupLayout";
 import ByDay from "@/components/lineup/ByDay";
 
 export default async function Days() {
