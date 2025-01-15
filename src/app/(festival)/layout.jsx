cc68eecb35aed054d65586042d1b0a599e99984b
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-01-15 10:02:46 +0100

    .

--- src/app/(festival)/layout.jsx
+++ src/app/(festival)/layout.jsx
@@ -13,8 +13,6 @@ export const metadata = {
   title: "FooFest",
 };
 
-import Header from "@/components/Header";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={anton.variable}>
