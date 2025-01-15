72634c779643240a391bcd093933dd10b5fd6ec2
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-01-15 21:39:40 +0100

    Filter

--- src/app/(festival)/lineup/artists/page.jsx
+++ src/app/(festival)/lineup/artists/page.jsx
@@ -1,60 +1,76 @@
-import { getArtists } from "@/lib/lineup";
-import { endpointAPI } from "@/lib/endpoints";
+// COMPONENTS
 import Link from "next/link";
-import ScrollToButton from "@/components/lineup/ScrollToButton";
 import ArtistCard from "@/components/lineup/ArtistCard";
+import { ScrollToButton } from "@/components/lineup/Buttons";
+// FUNCTIONS
+import { getArtists } from "@/lib/lineup";
+import Filter from "@/components/lineup/SortByMenu";
 
-export default async function Artists({ searchParams }) {
-  const { limit, genre } = await searchParams;
+async function artistData(genre, limit) {
+  let artists = await getArtists();
+
+  if (genre) {
+    artists.filter((artist) => artist.genre === genre);
+  }
+
+  return artists.sort((a, b) => a.name.localeCompare(b.name)).slice(0, limit);
+}
+
+async function genreData() {
   const artists = await getArtists();
+  const genres = new Set(artists.map((artist) => artist.genre).sort());
+
+  return [...genres];
+}
+
+export default async function Page({ searchParams }) {
+  const { genre, limit } = await searchParams;
+  const genres = await genreData();
+  let artists = await artistData(genre, limit || 12);
+
+  console.log(artists);
 
   return (
-    <section className="grid grid-cols-4 gap-4">
-      <ul className="grid grid-cols-subgrid col-span-full gap-y-4 ">
-        {artists.map((artist, i) => (
-          <ArtistCard key={i} {...artist} />
-        ))}
+    <section className="grid grid-cols-4 gap-4 items-start">
+      <Filter genres={genres} active={genre} />
+      <ul className="col-span-3 grid grid-cols-subgrid gap-4">
+        {genre
+          ? genre.map((filter, id) => (
+              <FilterGroup key={id} filter={filter}>
+                {artists
+                  .filter((artist) => artist.genre === filter)
+                  .map((artist, id) => (
+                    <ArtistCard key={id} {...artist} />
+                  ))}
+              </FilterGroup>
+            ))
+          : artists.map((artist, id) => <ArtistCard key={id} {...artist} />)}
       </ul>
-      <footer className="col-span-full">
-        {/* {limit < artists.length && (
-        <Link
-          href={`/lineup/artists?limit=${newLoad}`}
-          scroll={false}
-          className="border-2 border-forest-600 body-copy text-forest-500 hover:text-forest-400 hover:border-forest-500 inline-block px-6 py-3 mt-8 place-self-center"
-        >
-          Load more
-        </Link>
+      <Link
+        href={`/lineup/artists?limit=${Number(limit) + 12 || 24}${
+          genre ? `&genre=${genre.join("&genre=")}` : ""
+        }`}
+        scroll={false}
+        className="col-start-2 col-span-3 cursor-pointer border-2 border-forest-600 body-copy text-forest-500 hover:text-forest-400 hover:border-forest-500 inline-block px-6 py-3 mt-8 place-self-center"
+      >
+        Load more
+      </Link>
+      {/* {limit < artists.length && (
       )} */}
-      </footer>
-
-      {/* <ScrollToButton scrollFromTop="0" simple={false}>
+      <ScrollToButton scrollFromTop="0" simple={false}>
         Back to top
-      </ScrollToButton> */}
+      </ScrollToButton>
     </section>
   );
 }
 
-const ByArtist = ({ artists, limit }) => {
-  //Sorting artists
-  // Useing method from the following link
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-  const artistsSortedByName = artists.sort((a, b) => {
-    const nameA = a.name.toUpperCase();
-    const nameB = b.name.toUpperCase();
-
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  });
-
-  // For "load more" button
-  const initalLoad = 12;
-  const currentLoad = limit ? limit : (limit = initalLoad);
-  const newLoad = Number(currentLoad) + initalLoad;
-
-  const artistsShowed = artistsSortedByName.slice(0, limit);
-};
+function FilterGroup({ filter, children }) {
+  return (
+    <li className="col-span-3 grid grid-cols-subgrid">
+      <h2 className="col-span-full heading-4">{filter}</h2>
+      <ul className="col-span-full grid grid-cols-subgrid gap-y-4 py-6">
+        {children}
+      </ul>
+    </li>
+  );
+}
