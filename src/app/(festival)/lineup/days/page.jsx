943b8296ba38548975c9fe3581828e205159459a
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-01-15 10:08:43 +0100

    Days fixed

--- src/app/(festival)/lineup/days/page.jsx
+++ src/app/(festival)/lineup/days/page.jsx
@@ -1,9 +1,31 @@
-import { getStages } from "@/lib/lineup";
+import Accordion from "@/components/Accordion";
+import ColSchedule from "@/components/lineup/Table";
+import { stageSchedule, Week } from "@/lib/schedule";
 
-import ByDay from "@/components/lineup/ByDay";
+export default async function Days({ searchParams }) {
+  const { day } = await searchParams;
+  const schedule = await stageSchedule();
+  const days = Week(schedule);
 
-export default async function Days() {
-  const stages = await getStages();
-
-  return <ByDay data={stages} />;
+  return (
+    <section className="grid gap-4">
+      {days.map((obj, i) => {
+        return (
+          <Accordion
+            key={i}
+            variant="primary"
+            label={obj.name}
+            name="day"
+            isOpen={day === obj.name}
+          >
+            <section className="grid grid-cols-[auto_1fr_1fr_1fr] grid-rows-[2fr_12fr]">
+              {obj.cols.map((stage, i) => {
+                return <ColSchedule {...stage} key={i} />;
+              })}
+            </section>
+          </Accordion>
+        );
+      })}
+    </section>
+  );
 }
