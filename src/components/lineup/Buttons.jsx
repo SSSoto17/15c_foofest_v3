236b286b70e27e4c1070facd07a429b6771c7bcf
Sif Melkane <siha0004@stud.kea.dk>
2025-01-16 22:20:12 +0100

    Deleting unused useRouter

--- src/components/lineup/Buttons.jsx
+++ src/components/lineup/Buttons.jsx
@@ -2,17 +2,13 @@
 import Link from "next/link";
 import { Button } from "@headlessui/react";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { MdOutlineArrowBack, MdOutlineArrowUpward } from "react-icons/md";
 
 export const BackButton = () => {
   const router = useRouter();
 
   return (
-    <Button
-      onClick={() => router.back()}
-      className="cursor-pointer body-copy uppercase font-bold py-4 transition-all duration-150 text-aztec-400  hover:text-aztec-300 hover:scale-105 flex gap-2 items-center"
-    >
+    <Button onClick={() => router.back()} className="cursor-pointer body-copy uppercase font-bold py-4 transition-all duration-150 text-aztec-400  hover:text-aztec-300 hover:scale-105 flex gap-2 items-center">
       <MdOutlineArrowBack />
       Back
     </Button>
@@ -53,30 +49,19 @@ export function ScrollToButton({ scrollFromTop }) {
   }, []);
 
   return (
-    <button
-      aria-label="Back to Top"
-      className={`fixed m-12 bottom-0 right-0 rounded-full bg-forest-600 drop-shadow-2xl body-copy font-bold p-4 z-30 cursor-pointer hover:scale-105 transition-all duration-150 opacity-0 ${
-        isVisible && "opacity-100"
-      }`}
-      onClick={scrollToTop}
-    >
+    <button aria-label="Back to Top" className={`fixed m-12 bottom-0 right-0 rounded-full bg-forest-600 drop-shadow-2xl body-copy font-bold p-4 z-30 cursor-pointer hover:scale-105 transition-all duration-150 opacity-0 ${isVisible && "opacity-100"}`} onClick={scrollToTop}>
       <MdOutlineArrowUpward size="24" />
     </button>
   );
 }
 
 export function LoadMore({ limit }) {
-  const router = useRouter();
   const loadLimit = limit ? Number(limit) + 12 : 24;
   const newLimit = `/lineup/artists?limit=${loadLimit}`;
 
   return (
     <footer className="grid place-content-center col-span-full">
-      <Link
-        href={newLimit}
-        scroll={false}
-        className="button button-secondary button-size-base font-bold uppercase"
-      >
+      <Link href={newLimit} scroll={false} className="button button-secondary button-size-base font-bold uppercase">
         Load More
       </Link>
     </footer>
